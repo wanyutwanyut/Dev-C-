5862f9ba4121308f29e1e93c365b7bcdacf850c2
wanyutwanyut <97231201+wanyutwanyut@users.noreply.github.com>
2022-01-06 20:32:03 +0800

    Add files via upload

---
+++ hello world.cpp
@@ -0,0 +1,11 @@
+//范例1-1
+//Hello World 程序
+//实现“Hello，world!”输出
+//2017.07.10
+#include <iostream>
+using namespace std;
+int main()                         //定义主函数
+{
+	cout<<"Hello，world!“;
+	return 0;
+}
